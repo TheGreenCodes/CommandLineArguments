22d161085b9c45d0ffb52992de654fed0800bcef
MarvinKweyu <mkweyu1@gmail.com>
2020-02-16 18:56:46 +0300

    Add Part 3 and modify readme to reflect

---
+++ Part3/advanced_square.py
@@ -0,0 +1,53 @@
+import getopt
+import sys
+from math import sqrt
+
+"""
+get the square but get the square root incase the argument 'root' is provided
+"""
+
+
+def usage():
+    """
+    Show help for the CLI program
+    """
+    print("python advanced_square.py --number <your_number> \n OR\n")
+    print("python advanced_square.py -n <your_number>\n")
+    print("To get the square root: python advanced_square.py -n <your_number> -r")
+    print("Example: get the square\n\tpython advanced_square.py -n 5")
+
+
+def main():
+    
+    try:
+        option, arguments = getopt.getopt(sys.argv[1:],"hn:r",["help","number=","root"])
+    except getopt.GetoptError as error:
+        print(error)
+        sys.exit()
+
+
+    number = None
+    root_number = False
+
+
+    for opt, variable in option:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif opt in ("-n", "--number"):
+            number = int(variable)
+        elif opt in ('-r','--root'):
+            root_number = True
+        else:
+            usage()
+            sys.exit()
+   
+    if root_number:
+        print(f"The square root of {number} = {sqrt(number)}")
+    else:
+        print(f"The square of {number} = {number* number} ")
+    
+
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ A walk through python command-line arguments
 
  - [Part 1](https://marvin.hashnode.dev/python-command-line-arguments-part-1-ck2gd5o7400fewks155jr7isq)
  - [Part 2](https://marvin.hashnode.dev/command-line-arguments-part-2-ck2urwww5006o49s1flu1ntkh)
- - Part 3 (coming soon!)
+ - [Part 3] (https://marvin.hashnode.dev/python-command-line-arguments-part-3-ck6p7lheb02med9s1crh04zem)
